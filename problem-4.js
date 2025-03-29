db4395bd9e60dc59abe8b4b90a29fc29c1ef8dc2
hmmasudsarder <hmmasudsardar@gmail.com>
2025-03-29 17:03:09 +0600

    added thrad problem

--- problem-4.js
+++ problem-4.js
@@ -1,9 +1,9 @@
 const cars = [
-    { make: 'Toyota', model: 'Camry', year: 2015 },
+    { make: 'Toyota', model: 'Camry', year: 2025 },
     { make: 'Honda', model: 'Civic', year: 2018 },
-    { make: 'Ford', model: 'Mustang', year: 2020 }
+    { make: 'Ford', model: 'Mustang', year: 2000 }
   ];
   
   const sortedCars = cars.sort((a, b) => a.year - b.year);
-
+  console.log(sortedCars);
   
