4283643cf608f77bc1fa33c88240ac9f98caf6f7
hmmasudsarder <hmmasudsardar@gmail.com>
2025-04-01 16:36:00 +0600

    added first problem

---
+++ problem-1.js
@@ -0,0 +1,10 @@
+const people = [
+    { name: 'masud', age: 18, gender: 'male' },
+    { name: 'sadia', age: 15, gender: 'female' },
+    { name: 'rakib', age: 25, gender: 'male' },
+    { name: 'farzana', age: 22, gender: 'female' }
+  ];
+  
+  const filteredNames = people.filter(person => person.gender !== 'female').map(person => person.name);
+  console.log(filteredNames);
+  
